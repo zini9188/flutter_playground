5c6e15989c4716e43e9a060658009515c0a2ff99
zqwz <zqwz@naver.com>
2024-03-31 17:09:03 +0900

    feat: add getTodayWebtoos API

---
+++ lib/services/api_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter_playground/models/webtoon_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final resposne = await http.get(url);
+    if (resposne.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(resposne.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
